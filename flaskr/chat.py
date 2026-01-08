7181e5ebf43fc9e619c85cc8c9646c06560ef3e4
Franco <frfantusatti@protonmail.com>
2026-01-08 03:06:37 -0300

    change sqlite for postgress for deploy

--- flaskr/chat.py
+++ flaskr/chat.py
@@ -1,41 +1,40 @@
 import uuid
-
 from flask import Blueprint, jsonify, render_template, request, session
-
 from flaskr.bot import bot_response
 from flaskr.db import get_db
 
 bp = Blueprint("chat", __name__)
 
-
 @bp.route("/chat", methods=["GET"])
 def index():
     if "session_id" not in session:
         session["session_id"] = str(uuid.uuid4())
-
+    
     db = get_db()
-    messages = db.execute(
-        "SELECT message, response FROM chat WHERE session_id = ?",
-        (session["session_id"],),
-    ).fetchall()
-
+    cursor = db.cursor()
+    cursor.execute(
+        "SELECT message, response FROM chat WHERE session_id = %s",
+        (session["session_id"],)
+    )
+    messages = cursor.fetchall()
+    
     return render_template("chat/index.html", messages=messages)
-
 
 @bp.route("/chat", methods=["POST"])
 def chat():
     if "session_id" not in session:
         session["session_id"] = str(uuid.uuid4())
-
+    
     datos = request.get_json()
     msg = datos["texto"]
     reply = bot_response(msg)
-
+    
     db = get_db()
-    db.execute(
-        "INSERT INTO chat (message, response, session_id) VALUES (?, ?, ?)",
-        (msg, reply, session["session_id"]),
+    cursor = db.cursor()
+    cursor.execute(
+        "INSERT INTO chat (message, response, session_id) VALUES (%s, %s, %s)",
+        (msg, reply, session["session_id"])
     )
     db.commit()
-
+    
     return jsonify({"resultado": reply})
