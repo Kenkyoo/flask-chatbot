bd884e670c1b477b6265e3480739920b5e5e1617
Franco <frfantusatti@protonmail.com>
2026-01-08 04:29:30 -0300

    set db.py

--- flaskr/db.py
+++ flaskr/db.py
@@ -1,21 +1,16 @@
-import psycopg2
-import psycopg2.extras
-from datetime import datetime
+import psycopg
 import click
 from flask import current_app, g
 
 def get_db():
     if "db" not in g:
-        g.db = psycopg2.connect(
-            current_app.config["DATABASE_URL"]
-        )
+        g.db = psycopg.connect(current_app.config["DATABASE_URL"])
     return g.db
 
 def init_db():
     db = get_db()
     with current_app.open_resource("schema.sql") as f:
-        cursor = db.cursor()
-        cursor.execute(f.read().decode("utf8"))
+        db.execute(f.read().decode("utf8"))
         db.commit()
 
 @click.command("init-db")
@@ -23,11 +18,11 @@ def init_db_command():
     init_db()
     click.echo("Initialized the database.")
 
-def init_app(app):
-    app.teardown_appcontext(close_db)
-    app.cli.add_command(init_db_command)
-
 def close_db(e=None):
     db = g.pop("db", None)
     if db is not None:
         db.close()
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
